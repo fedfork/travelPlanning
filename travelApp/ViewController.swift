4b40c7de8ce9b4138d667345471a79ca6e58fd5b
Fedork <very.dark.azure@gmail.com>
2020-02-02 20:54:49 +0300

    Initial Commit

---
+++ travelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travelApp
+//
+//  Created by Fedor Korshikov on 02.02.2020.
+//  Copyright © 2020 Fedor Korshikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
